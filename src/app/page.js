b1cd232a81343ad764668c24a1f571f3596e8fbd
Guilherme <guilherm.rodrigues@outlook.com>
2024-12-19 20:24:33 -0300

    primeiro commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main>
+        <h1>ola mundo</h1>
+      </main>
+    </div>
+  );
+}
